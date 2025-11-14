de3e64302fd7af197fbebde30ae634779685d381
Gian <gianalbertmendaro@gmail.com>
2025-11-14 13:41:46 +0800

    security4commit

--- intro/index.php
+++ intro/index.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Front Page</title>
-  <link rel="stylesheet" href="front.css">
+  <link rel="stylesheet" href="design/front.css">
 </head>
 <body>
   <?php include 'header.php'; ?>
